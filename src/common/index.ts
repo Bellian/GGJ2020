d541c879c8b37a0d4ae72ef6d8b1fdb8ce24bfc9
Sven Kernke <sven.kernke@yahoo.de>
2020-02-01 00:55:02 +0100

    server
    stuff

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,16 +1,44 @@
+declare const AirConsole: any;
 export const Module = "common";
 
 export class Server {
   airConsole = new AirConsole();
 }
 
+export class Client {
+  airconsole: AirConsole;
+  playerData: PlayerData;
+  constructor(data: { id: number }) {
+    this.airconsole = new AirConsole();
+    this.playerData = new PlayerData();
+    this.subscribeToAirConsole();
+  }
+
+  send() {
+    data.id = this.airconsole.getDeviceId() as number;
+    this.airconsole.message(AirConsole.SCREEN, JSON.stringify(data));
+  }
+
+  recive() {}
+
+  subscribeToAirConsole() {
+    //  this.onMessage();
+  }
+
+  // onMessage(){
+  // this.airconsole.onMessage =>(from, data) {
+  //   var info = document.createElement('DIV');
+  //   info.innerHTML = data;
+  //   document.body.appendChild(info);
+  // };}
+}
+
 export class PlayerData {
   type: TransactionType = TransactionType.PlayerData;
-  x: number;
-  y: number;
-  id: number;
-  playerState: PlayerState;
-  isAngryDad: boolean;
+  x: number = 0;
+  y: number = 0;
+  playerState: PlayerState = PlayerState.idle;
+  isAngryDad: boolean = false;
 }
 
 export enum TransactionType {
@@ -26,19 +54,12 @@ export enum ServerState {
 }
 
 export class ControllerData {
-  type: TransactionType = TransactionType.ControllerData;
-  x: number;
-  y: number;
-  id: number;
-}
-
-export class PositionData {
-  x: number;
-  y: number;
-  id: number;
+  x: number = 0;
+  y: number = 0;
 }
 
 export enum PlayerState {
+  idle,
   dead,
   running,
   interacting
