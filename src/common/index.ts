f89f6122672e82e254fdb7a1c3f79dfd6c67995d
Sven Kernke <sven.kernke@yahoo.de>
2020-01-31 23:48:32 +0100

    server
    init classes

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,1 +1,45 @@
-export const Module = 'common';
+export const Module = "common";
+
+export class Server {
+  airConsole = new AirConsole();
+}
+
+export class PlayerData {
+  type: TransactionType = TransactionType.PlayerData;
+  x: number;
+  y: number;
+  id: number;
+  playerState: PlayerState;
+  isAngryDad: boolean;
+}
+
+export enum TransactionType {
+  ServerState,
+  PlayerData,
+  ControllerData
+}
+
+export enum ServerState {
+  joining,
+  running,
+  final
+}
+
+export class ControllerData {
+  type: TransactionType = TransactionType.ControllerData;
+  x: number;
+  y: number;
+  id: number;
+}
+
+export class PositionData {
+  x: number;
+  y: number;
+  id: number;
+}
+
+export enum PlayerState {
+  dead,
+  running,
+  interacting
+}
