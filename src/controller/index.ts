483da3b13b2cf79e83ac9d5d9feee56643a12578
Noel <schenknoel@gmail.com>
2020-02-01 15:29:00 +0100

    can be tested

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,6 +1,6 @@
 import { ControllerData, Client, ServerState, Server, ServerData } from '../common/index';
 
-enum Views {
+export enum Views {
     splashscreen,
     characterselection,
     playscreen,
@@ -10,10 +10,7 @@ enum Views {
 class Controller{
     private startPos:[number, number] | undefined = undefined;
     constructor(private client:Client){
-        document.addEventListener('DOMContentLoaded', () => {
-            this.virtualController();
-            this.client.onUpdateServerData(this.updateView.bind(this));
-        });
+        this.client.onUpdateServerData(this.updateView.bind(this));
     }
 
     showView(view:Views){
@@ -28,15 +25,18 @@ class Controller{
         switch(serverData.serverState){
             case ServerState.lobby:
                 this.showView(Views.splashscreen);
+                this.lobby();
                 break;
             case ServerState.characterSelection:
-                this.showView(Views.characterselection)
+                this.showView(Views.characterselection);
+                this.characterselection();
                 break;
             case ServerState.running:
-                this.showView(Views.playscreen)
+                this.showView(Views.playscreen);
+                this.virtualController();
                 break;
             case ServerState.final:
-                this.showView(Views.endscreen)
+                this.showView(Views.endscreen);
                 break;
             default:
                 console.error('not implemented', serverData);
@@ -44,41 +44,52 @@ class Controller{
     }
 
     virtualController(){
-        document.querySelectorAll('controller')[0].addEventListener('touchstart', (ev) => {
-            console.log('touchstart');
+        document.querySelectorAll('playscreen > controller')[0].addEventListener('touchstart', (ev) => {
             this.startPos = [(ev as TouchEvent).targetTouches[0].clientX, (ev as TouchEvent).targetTouches[0].clientY];
         });
-        document.querySelectorAll('controller')[0].addEventListener('touchmove', (ev) => {
+        document.querySelectorAll('playscreen > controller')[0].addEventListener('touchmove', (ev) => {
             if(this.startPos !== undefined) {
                 this.client.sendControllerData(new ControllerData((ev as TouchEvent).targetTouches[0].clientX - this.startPos[0], (ev as TouchEvent).targetTouches[0].clientY - this.startPos[1]));
             }
         });
-        document.querySelectorAll('controller')[0].addEventListener('touchend', (ev) => {
+        document.querySelectorAll('playscreen > controller')[0].addEventListener('touchend', (ev) => {
             this.startPos = undefined;
         });
     }
 
-    splashscreen(){
-        document.querySelectorAll('splashscreen > button').forEach( btn => {
-            let character = document.querySelectorAll('splashscreen > character')[0];
+    lobby(){
+        this.setTime(Views.splashscreen);
+    }
+
+    characterselection(){
+        document.querySelectorAll('characterselection > button').forEach( btn => {
             let isPlayerAngryDad = this.client.toggleAngryDad();
             if (isPlayerAngryDad){
-                character.innerHTML = 'You would like to be angry dad!';
+                document.querySelectorAll('characterselection')[0].classList.add('isAngryDad');
+                console.log('You would like to be angry dad!');
             }else{
-                character.innerHTML = 'You would like to be a wichtel!';
+                document.querySelectorAll('characterselection')[0].classList.remove('isAngryDad');
+                console.log('You would like to be a wichtel!');
             }
         });
-        let timeUntilStartInterval = setInterval(() => {
-            let timeUntilStart = this.client.getTime();
-            document.querySelectorAll('splashscreen > time')[0].innerHTML = timeUntilStart.toString();
-            if(timeUntilStart === 0){
-                clearInterval(timeUntilStartInterval);
+        this.setTime(Views.characterselection);
+    }
+
+    setTime(view:Views){
+        let timeUntilInterval = setInterval(() => {
+            let timeUntil = this.client.getTime();
+            document.querySelectorAll(Views[view] + ' > time')[0].innerHTML = timeUntil.toString();
+            if(timeUntil === 0){
+                clearInterval(timeUntilInterval);
             }
         }, 1000);
     }
 }
 
 
-
-let client:Client = new Client();
-new Controller(client);
+document.addEventListener('DOMContentLoaded', () => {
+    let client:Client = new Client();
+    let controller = new Controller(client);
+    let test = new ServerData(30, ServerState.lobby);
+    controller.updateView(test);
+});
